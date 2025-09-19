1f81b37359fd6b537565ece49a6b216464d9cdab
Jesus Enríquez Jaime <jesus-e98@hotmail.com>
2025-09-19 16:18:19 -0700

    ⚡️ Reduce 300kb bundle size

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import {
+  CellStyleModule,
+  ClientSideRowModelModule,
+  DateFilterModule,
+  ModuleRegistry,
+} from "ag-grid-community";
 
 // Register all Community features
-ModuleRegistry.registerModules([AllCommunityModule]);
+ModuleRegistry.registerModules([
+  ClientSideRowModelModule,
+  DateFilterModule,
+  CellStyleModule,
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
